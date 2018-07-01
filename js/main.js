f535fe6a3451c8c4ed67655519089a033f7cc6e6
Michał Bar <logout400@gmail.com>
2018-07-01 18:29:25 +0200

    fix: Make restaurant list mobile-friendly

--- js/main.js
+++ js/main.js
@@ -163,24 +163,28 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const description = document.createElement('div');
+  description.className = 'restaurant-desc';
+  li.append(description);
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  description.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  description.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  description.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  description.append(more);
 
-  return li
+  return li;
 }
 
 /**
